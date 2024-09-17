61fad24177140c6e02bd3277ab12585b94bec220
Vaishnvai Gupta <vaishnavivaishnavigupta2204@gmail.com>
2024-09-17 15:59:05 +0530

    done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,5 @@
 import CoverImg from '../asset/cover_img.jpeg';
-import React, { useState } from "react";
-import axios from 'axios';
+
 
 const colors = {
   primary: '#060606',
@@ -9,60 +8,7 @@ const colors = {
 }
 
 const Login = () => {
-    const [mobileNumber, setMobileNumber] = useState("");
-    const [otp, setOtp] = useState('');
-    const [isOtpSent, setIsOtpSent] = useState(false);
-    const [isOtpVerified, setIsOtpVerified] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    // function to send OTP
-    const sendOtp = async () => {
-      if (!mobileNumber || mobileNumber.length !== 10) {
-        setErrorMessage('Please enter a valid 10-digit mobile number.');
-        return;
-      }
-
-      try {
-        const response = await axios.post('http://localhost:5000/send-otp', { mobileNumber });
-        if (response.data.success) {
-          setIsOtpSent(true);
-          setErrorMessage('');
-        } else {
-          alert('Failed to send OTP');
-        }
-      } catch (error) {
-        console.error('Error sending OTP', error);
-        setErrorMessage('error sending OTP.')
-      }
-    };
-
-    // function to verify OTP
-    const verifyOtp = async () => {
-      try {
-        const response = await axios.post('http://localhost:5000/verify-otp', { mobileNumber, otp });
-        if (response.data.success) {
-          setIsOtpVerified(true);
-          setErrorMessage('');
-          alert('OTP Verified Successfully!');
-        } else {
-          alert('Invalid OTP');
-        }
-      } catch (error) {
-        console.error('Error verifying OTP', error);
-        setErrorMessage('Error verifying OTP.');
-
-      }
-    };
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Submit logic for the mobile number
-      console.log("Mobile Number Submitted:", mobileNumber);
-    };
-  
   return (
-  
-    <form onSubmit={handleSubmit} className="mobile-number-form">
     <div className='w-full h-screen flex items-start'>
       <div className='relative w-1/2 h-full flex flex-col'>
         <div className='absolute top-[25%] left-[10%] flex flex-col'>
@@ -82,34 +28,13 @@ const Login = () => {
           </div>
            
           <div className='w-full flex flex-col'>
-            {errorMessage && <p style={{color:'red'}}> {errorMessage}</p>}
-            {!isOtpSent ? (
-            <>
-             <input 
+            <input 
             type="tel"
             placeholder="Mobile Number"
-            value={mobileNumber}
-            onChange={(e) => setMobileNumber(e.target.value)}
-            required
             className='w-full text-black py-2 my-4 bg-transparent border-b border-black outline-none focus:outline-none'/>  
-             <button onClick={sendOtp} className='w-full my-2 items-center justify-center'>Send OTP</button>
-           </>
-            ) : (
-              <>
-              <input
-            type="text"
-            placeholder="Enter OTP"
-            value={otp}
-            onChange={(e) => setOtp(e.target.value)}
-          />
-          <button onClick={verifyOtp}>Verify OTP</button>
-          </>
-          )}
-          {isOtpVerified && <p>Welcome! You are logged in.</p>}
-
           </div>
           <div  className='w-full flex flex-col'>
-           <button type='submit' className='w-full text-white my-2 bg-[#060606] rounded-md p-4 text-center flex items-center justify-center hover:opacity-75'>
+           <button className='w-full text-white my-2 bg-[#060606] rounded-md p-4 text-center flex items-center justify-center hover:opacity-75'>
              Continue
            </button>
           </div>
@@ -119,7 +44,6 @@ const Login = () => {
         </div>
       </div>
     </div>
-    </form>
   )
 }
 export default Login;
